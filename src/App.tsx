f1c1eca07e824958b5000f4d7b99afced1a070af
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-06-21 20:30:29 +0200

    color theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import './App.css'
 import { AdvertsProvider } from './lib/adverts/AdvertsContext'
 import { createAdvertsRepository } from './lib/adverts/adverts-repository'
 import { AppRouter } from './routes/AppRouter'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material/styles'
 
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: 'rgb(80, 129, 27)',
+		},
+		secondary: {
+			main: 'rgb(225, 233, 219)',
+		},
+	},
+})
 const App: FC = () => {
 	const [repository] = useState(createAdvertsRepository())
 	return (
 		<AdvertsProvider repository={repository}>
-			<AppRouter/>
+			<ThemeProvider theme={theme}>
+				<AppRouter/>
+			</ThemeProvider>
 		</AdvertsProvider>)
 }
 
