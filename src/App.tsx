b7cb1b3fafef78f7d595a3ca76133056a82e6a88
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-06-21 10:22:23 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React, { FC, useState } from 'react'
 import './App.css'
 import { AdvertsProvider } from './lib/adverts/AdvertsContext'
 import { createAdvertsRepository } from './lib/adverts/adverts-repository'
-import Main from './main'
+import Main from './screens/main'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Home } from './screens'
+
+
+const createRouter = () => createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<Home />}>
+		</Route>
+	)
+)
 
 const App: FC = () => {
 	const [repository] = useState(createAdvertsRepository())
+	const [router] = useState(createRouter())
 	return (
 		<AdvertsProvider repository={repository}>
-			<Main>tjiongeling</Main>
+			<Main>
+				<RouterProvider router={router} />
+			</Main>
 		</AdvertsProvider>)
 }
 
