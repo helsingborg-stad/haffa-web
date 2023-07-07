33243ddbcc6ea0bf81cc98eba71295cee1edd672
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-07-07 17:29:43 +0200

    serverside catchall to fix clientside routing

--- index.js
+++ index.js
@@ -8,8 +8,10 @@
 
 require('dotenv').config()
 const Koa = require('koa')
+const send = require('koa-send')
 const serve = require('koa-static')
 const proxy = require('koa-proxies')
+const path = require('path')
 
 const PORT = process.env.PORT ?? 3000
 const backendUrl = process.env.HAFFA_BACKEND_URL
@@ -24,5 +26,10 @@ if (backendUrl) {
     })
   )
 }
+
+// Catch all to compensate for react-router-dom routing
+// see https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually
+app.use(async (ctx) => send(ctx, path.join('.', 'build', 'index.html')))
+
 
 app.listen(PORT, () => console.log(`Haffa web running on port ${PORT}`))
