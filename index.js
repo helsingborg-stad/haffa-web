e17a83cd06477228c0b6bd798e2c225271103696
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-09-27 15:21:46 +0200

    proxy and asset serving tidy

--- index.js
+++ index.js
@@ -12,15 +12,18 @@ const Router = require('@koa/router')
 const send = require('koa-send')
 const serve = require('koa-static')
 const proxy = require('koa-proxies')
-const path = require('path')
-
+const { join } = require('path')
+const { readFile } = require('fs/promises')
 const PORT = process.env.PORT ?? 3000
 const backendUrl = process.env.HAFFA_BACKEND_URL
 const app = new Koa()
 const router = new Router()
 
-app.use(serve('./build'))
-
+/************************************************************
+ *
+ * Proxy requests to backend
+ *
+ ***********************************************************/
 if (backendUrl) {
     app.use(
         proxy('/api', {
@@ -29,21 +32,64 @@ if (backendUrl) {
     )
 }
 
-// publish theme settings to client
+/************************************************************
+ *
+ * Annotate every response with Git revision
+ *
+ ***********************************************************/
+const revision = readFile(join(process.cwd(), 'git_revision.txt'), {
+    encoding: 'utf8',
+})
+    .then(
+        (text) =>
+            (text || '')
+                .split('\n')
+                .map((v) => v.trim())
+                .filter((v) => v)[0] || ''
+    )
+    .catch(() => '')
+
+app.use(async (ctx, next) => {
+    const r = await revision
+    r && ctx.set('x-git-rev', r)
+    return next()
+})
+
+/************************************************************
+ *
+ * Serve React app as static files
+ *
+ ***********************************************************/
+app.use(serve('./build'))
+
+/************************************************************
+ *
+ * Publish custom branding to client
+ *
+ ***********************************************************/
 const brandingPath = process.env.BRANDING_PATH || ''
 router.get(
     '/branding.json',
     brandingPath
-        ? (ctx) => send(ctx, path.join('.', brandingPath), { hidden: true })
+        ? (ctx) => send(ctx, join('.', brandingPath), { hidden: true })
         : (ctx) => {
               ctx.body = {}
           }
 )
 
-// Catch all to compensate for react-router-dom routing
-// see https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually
-router.get('/:path*', (ctx) => send(ctx, path.join('.', 'build', 'index.html')))
+/************************************************************
+ *
+ * Catch all to compensate for react-router-dom routing
+ * see https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually
+ *
+ ***********************************************************/
+router.get('/:path*', (ctx) => send(ctx, join('.', 'build', 'index.html')))
 
+/************************************************************
+ *
+ * Register routes and start webserver
+ *
+ ***********************************************************/
 app.use(router.routes())
 app.use(router.allowedMethods())
 
