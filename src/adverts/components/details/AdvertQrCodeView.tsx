58c03e655c2cc96381b5092b89dbefe29c5a5650
Petter Andersson <petter.andersson@helsingborg.se>
2023-10-03 09:32:12 +0200

    fixes for  printing

--- src/adverts/components/details/AdvertQrCodeView.tsx
+++ src/adverts/components/details/AdvertQrCodeView.tsx
@@ -1,11 +1,21 @@
+import { styled } from '@mui/material'
+import Box from '@mui/material/Box'
+import Slider from '@mui/material/Slider'
 import { Advert } from 'adverts'
 import { DeepLinkContext } from 'deep-links/DeepLinkContext'
-import { FC, useContext } from 'react'
+import { FC, useContext, useState } from 'react'
 import QRCode from 'react-qr-code'
+
+const NonPrintableComponent = styled('div')({
+    '@media print': {
+        display: 'none',
+    },
+})
 
 export const AdvertQrCodeView: FC<{ advert: Advert }> = ({ advert }) => {
     const { getAdvertLinkForQrCode } = useContext(DeepLinkContext)
     const link = getAdvertLinkForQrCode(advert)
+    const [size, setSize] = useState<number>(55)
     console.log(link)
     return (
         <div
@@ -16,8 +26,22 @@ export const AdvertQrCodeView: FC<{ advert: Advert }> = ({ advert }) => {
                 height: 'auto',
             }}
         >
+            <NonPrintableComponent>
+                <Box sx={{ width: 300, padding: 5 }}>
+                    <Slider
+                        min={10}
+                        max={400}
+                        defaultValue={size}
+                        aria-label="Default"
+                        valueLabelDisplay="auto"
+                        onChange={(_, value) =>
+                            setSize(Array.isArray(value) ? value[0] : value)
+                        }
+                    />
+                </Box>
+            </NonPrintableComponent>
             <div>
-                <QRCode size={55} value={link} style={{}} />
+                <QRCode size={size} value={link} style={{}} />
             </div>
         </div>
     )
