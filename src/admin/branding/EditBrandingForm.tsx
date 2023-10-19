b868dac2f4d7f114e947cba9bd6d35fc9e992cfa
Petter Andersson <petter.andersson@helsingborg.se>
2023-10-19 11:06:40 +0200

    add phrases to branding

--- src/admin/branding/EditBrandingForm.tsx
+++ src/admin/branding/EditBrandingForm.tsx
@@ -1,17 +1,20 @@
 import { Button, Grid, Typography } from '@mui/material'
-import { FC, useState } from 'react'
+import { FC, useContext, useState } from 'react'
 import { Editorial } from 'editorials'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { SwatchesPicker } from 'react-color'
 import { themeDefaults } from 'branding/theme-factory'
 import SaveIcon from '@mui/icons-material/Save'
 import { getOption } from 'options'
+import { PhraseContext } from 'phrases'
 import type { Option } from '../../options/types'
 
 export const EditBrandingForm: FC<{
     options: Option[]
     onUpdate: (options: Option[]) => void
 }> = ({ options, onUpdate }) => {
+    const { phrase } = useContext(PhraseContext)
+
     const [[primary, setPrimary], [secondary, setSecondary]] = [
         useState(getOption('primary', options, themeDefaults.primary)),
         useState(getOption('secondary', options, themeDefaults.secondary)),
@@ -19,17 +22,23 @@ export const EditBrandingForm: FC<{
 
     return (
         <>
-            <Editorial>Definitioner för tema</Editorial>
+            <Editorial>
+                {phrase('THEME_EDITORIAL', 'Definitioner för tema')}
+            </Editorial>
             <Grid container direction="row" sx={{ paddingBottom: 5 }}>
                 <Grid key="primaryColor" item xs={12} sm={4} sx={{ p: 1 }}>
-                    <Typography>Primär färg</Typography>
+                    <Typography>
+                        {phrase('THEME_PROP_PRIMARY_COLOR', 'Primär färg')}
+                    </Typography>
                     <SwatchesPicker
                         color={primary}
                         onChangeComplete={(color) => setPrimary(color.hex)}
                     />
                 </Grid>
                 <Grid key="secondaryColor" item xs={12} sm={4} sx={{ p: 1 }}>
-                    <Typography>Sekundär färg</Typography>
+                    <Typography>
+                        {phrase('THEME_PROP_SECONDARY_COLOR', 'Sekundär färg')}
+                    </Typography>
                     <SwatchesPicker
                         color={secondary}
                         onChangeComplete={(color) => setSecondary(color.hex)}
@@ -53,7 +62,7 @@ export const EditBrandingForm: FC<{
                     ])
                 }
             >
-                Spara
+                {phrase('THEME_SAVE', 'Spara')}
             </Button>
             <Button
                 onClick={() => {
@@ -61,7 +70,7 @@ export const EditBrandingForm: FC<{
                     setSecondary(themeDefaults.secondary)
                 }}
             >
-                Återställ
+                {phrase('THEME_RESTORE', 'Återställ')}
             </Button>
         </>
     )
