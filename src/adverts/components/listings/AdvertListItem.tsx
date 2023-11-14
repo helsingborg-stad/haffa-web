a52ff78e6eb0107af603a293f1456ec08bdb1cbb
Petter Andersson <petter.andersson@helsingborg.se>
2023-11-14 23:47:11 +0100

    advertlisting improvement

--- src/adverts/components/listings/AdvertListItem.tsx
+++ src/adverts/components/listings/AdvertListItem.tsx
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom'
 import { Category } from 'categories/types'
 import { TreeAdapter } from 'lib/types'
 import { PhraseContext } from 'phrases'
+import RecyclingIcon from '@mui/icons-material/Recycling'
 import { Advert } from '../../types'
 
 const join = (sep: string, ...parts: (string | number | undefined)[]) =>
@@ -49,23 +50,41 @@ export const AdvertListItem: FC<{
                         />
                     </Grid>
                     <Grid item sx={{ flex: 1, m: 2 }}>
+                        <Typography
+                            variant="subtitle2"
+                            color="text.secondary"
+                            component="div"
+                        >
+                            {categoryLabel || 'Övrigt'}
+                        </Typography>
                         <Typography variant="h5" component="div">
                             {title}
                         </Typography>
                     </Grid>
                     <Grid item sx={{ mx: 2, mb: 2 }}>
-                        <Typography variant="subtitle1" component="div">
-                            {join(
-                                ', ',
-                                join(' ', reservableQuantity, unit),
-                                categoryLabel
-                            )}
+                        <Typography
+                            variant="caption"
+                            color="text.disabled"
+                            component="div"
+                        >
+                            {join(', ', join(' ', reservableQuantity, unit))}
                         </Typography>
-                        <Typography variant="subtitle1" component="div">
+                        <Typography
+                            variant="caption"
+                            color="text.disabled"
+                            component="div"
+                        >
                             {phrase(
                                 `ADVERT_TYPE_${advert.type.toUpperCase()}`,
                                 'Återbruk'
                             )}
+                            <RecyclingIcon
+                                sx={{
+                                    pl: 0.5,
+                                    fontSize: 18,
+                                    verticalAlign: 'middle',
+                                }}
+                            />
                         </Typography>
                     </Grid>
                 </Grid>
