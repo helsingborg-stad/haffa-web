86d57bddad3ace4ac1a82ae169eed6cf9cbf3cb7
Petter Andersson <petter.andersson@helsingborg.se>
2024-01-22 08:51:25 +0100

    dix urlparams with commas

--- src/url-params/url-params-context.ts
+++ src/url-params/url-params-context.ts
@@ -3,6 +3,9 @@ import { createUrlParamsAdapter } from './url-params-adapter'
 import { createDefaultUrlParamsMapper } from './url-params-mapper'
 import { UrlParamsAdapter, UrlParamsContextType } from './types'
 
+const encode = (arr?: string[]): string[] | undefined =>
+    arr?.map((v) => encodeURIComponent(v))
+
 const createUrlParamsContext = (
     adapter: UrlParamsAdapter
 ): UrlParamsContextType => ({
@@ -13,15 +16,19 @@ const createUrlParamsContext = (
         const categories = (params.c || '')
             .split(',')
             .map((s) => s.trim())
+            .map((s) => decodeURIComponent(s))
             .filter((v) => v)
         const tags = (params.tags || '')
             .split(',')
             .map((s) => s.trim())
+            .map((s) => decodeURIComponent(s))
             .filter((v) => v)
         const size = (params.size || '')
             .split(',')
             .map((s) => s.trim())
+            .map((s) => decodeURIComponent(s))
             .filter((v) => v)
+        console.log(params.size)
         const sorting = params.sf || ''
         const page = parseInt(params.p, 10) || 0
         return {
@@ -47,18 +54,18 @@ const createUrlParamsContext = (
                     sorting.field === filter.sorting?.field &&
                     sorting.ascending === filter.sorting?.ascending
             )?.key,
-            c: filter.fields?.category?.in?.join(','),
-            tags: filter.fields?.tags?.in?.join(','),
-            size: filter.fields?.size?.in?.join(','),
+            c: encode(filter.fields?.category?.in)?.join(','),
+            tags: encode(filter.fields?.tags?.in)?.join(','),
+            size: encode(filter.fields?.size?.in)?.join(','),
             p: filter.paging?.pageIndex,
         })
     },
     makeAdvertSubscriptionUrl: (basePath, filter) =>
         `${basePath}#${adapter.makeUrlParams('', {
             s: filter.search,
-            c: filter.categories?.join(','),
-            tags: filter.tags?.join(','),
-            size: filter.size?.join(','),
+            c: encode(filter.categories)?.join(','),
+            tags: encode(filter.tags)?.join(','),
+            size: encode(filter.size)?.join(','),
         })}`,
 })
 
