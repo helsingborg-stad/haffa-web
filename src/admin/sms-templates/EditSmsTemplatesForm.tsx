2ec74735d47e9bb2edf2a003d4a71cebdbed9475
Joakim Larsson <mimmertim@gmail.com>
2024-01-26 11:53:52 +0100

    sms editor layout changes

--- src/admin/sms-templates/EditSmsTemplatesForm.tsx
+++ src/admin/sms-templates/EditSmsTemplatesForm.tsx
@@ -1,12 +1,17 @@
 import {
-    Card,
-    CardContent,
-    CardHeader,
     FormControlLabel,
     FormGroup,
-    Stack,
+    Paper,
     Switch,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TableRow,
     TextField,
+    Typography,
 } from '@mui/material'
 import { AdminActionPanel } from 'components/AdminActionPanel'
 import { PhraseContext } from 'phrases'
@@ -25,7 +30,6 @@ export const EditSmsTemplatesForm: FC<{
             enabled,
         }))
     )
-
     const mutateTemplate = useCallback(
         (t: SmsTemplate, patch: Partial<SmsTemplate>) =>
             setMemo(
@@ -38,52 +42,79 @@ export const EditSmsTemplatesForm: FC<{
     const saveTemplates = useCallback(() => onUpdate(memo), [onUpdate, memo])
 
     return (
-        <Stack spacing={2}>
-            <AdminActionPanel onSave={saveTemplates} />
-            {memo.map((t) => (
-                <Card>
-                    <CardHeader title={t.templateId} />
-                    <CardContent>
-                        <FormGroup>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={t.enabled}
-                                        onChange={(e) =>
-                                            mutateTemplate(t, {
-                                                enabled: e.target.checked,
-                                            })
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell style={{ width: '1%' }}>Id</TableCell>
+                        <TableCell>
+                            {phrase('SMS_TEMPLATE_FIELD_ENABLED', 'Aktiv')}
+                        </TableCell>
+                        <TableCell>
+                            {phrase('SMS_TEMPLATE_FIELD_TEMPLATE', 'Mall')}
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={3}>
+                            <AdminActionPanel onSave={saveTemplates} />
+                        </TableCell>
+                    </TableRow>
+                </TableFooter>
+                <TableBody>
+                    {memo.map((t) => (
+                        <TableRow>
+                            <TableCell>
+                                <Typography style={{ whiteSpace: 'nowrap' }}>
+                                    {t.templateId}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <FormGroup>
+                                    <FormControlLabel
+                                        control={
+                                            <Switch
+                                                checked={t.enabled}
+                                                onChange={(e) =>
+                                                    mutateTemplate(t, {
+                                                        enabled:
+                                                            e.target.checked,
+                                                    })
+                                                }
+                                            />
                                         }
+                                        label={phrase(
+                                            'SMS_TEMPLATE_FIELD_ENABLED',
+                                            'Aktiv'
+                                        )}
                                     />
-                                }
-                                label={phrase(
-                                    'SMS_TEMPLATE_FIELD_ENABLED',
-                                    'Aktiv'
-                                )}
-                            />
-                        </FormGroup>
-                    </CardContent>
-                    <CardContent>
-                        <TextField
-                            label={phrase(
-                                'SMS_TEMPLATE_FIELD_TEMPLATE',
-                                'Mall'
-                            )}
-                            placeholder={phrase(
-                                'SMS_TEMPLATE_FIELD_TEMPLATE',
-                                'Mall'
-                            )}
-                            fullWidth
-                            multiline
-                            value={t.template}
-                            onChange={(e) =>
-                                mutateTemplate(t, { template: e.target.value })
-                            }
-                        />
-                    </CardContent>
-                </Card>
-            ))}
-            <AdminActionPanel onSave={saveTemplates} />
-        </Stack>
+                                </FormGroup>
+                            </TableCell>
+                            <TableCell>
+                                <TextField
+                                    label={phrase(
+                                        'SMS_TEMPLATE_FIELD_TEMPLATE',
+                                        'Mall'
+                                    )}
+                                    placeholder={phrase(
+                                        'SMS_TEMPLATE_FIELD_TEMPLATE',
+                                        'Mall'
+                                    )}
+                                    fullWidth
+                                    multiline
+                                    value={t.template}
+                                    onChange={(e) =>
+                                        mutateTemplate(t, {
+                                            template: e.target.value,
+                                        })
+                                    }
+                                />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
