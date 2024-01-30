be821a1c4ab0b0a4dec9b89714eb8fdbf29d5f81
Joakim Larsson <mimmertim@gmail.com>
2024-01-30 09:30:56 +0100

    sms preview advert selection

--- src/admin/sms-templates/EditSmsTemplatesForm.tsx
+++ src/admin/sms-templates/EditSmsTemplatesForm.tsx
@@ -1,4 +1,5 @@
 import {
+    Autocomplete,
     FormControlLabel,
     FormGroup,
     Paper,
@@ -13,10 +14,114 @@ import {
     TextField,
     Typography,
 } from '@mui/material'
+import { Advert, AdvertList, AdvertsContext } from 'adverts'
 import { AdminActionPanel } from 'components/AdminActionPanel'
 import { PhraseContext } from 'phrases'
-import { FC, useCallback, useContext, useState } from 'react'
+import { FC, useCallback, useContext, useEffect, useState } from 'react'
 import { SmsTemplate } from 'sms-templates'
+
+interface UseDebounceState<T> {
+    pending: boolean
+    value: T
+    enqueue: (key: any, next: () => Promise<T>) => void
+}
+const useDebounce = <T,>(
+    initialKey: any,
+    initial: T,
+    initialFetch: () => Promise<T>
+): UseDebounceState<T> => {
+    interface State {
+        key: any
+        value: T
+        current: Promise<any> | null
+        next: (() => Promise<T>) | null
+    }
+    const [state, setState] = useState<State>({
+        key: initialKey,
+        value: initial,
+        current: null,
+        next: initialFetch,
+    })
+
+    useEffect(() => {
+        const { current, next } = state
+        if (current) {
+            return
+        }
+        if (next) {
+            setState({
+                ...state,
+                next: null,
+                current: next().then((v) =>
+                    setState({
+                        ...state,
+                        value: v,
+                        current: null,
+                    })
+                ),
+            })
+        }
+    })
+
+    return {
+        pending: state.current != null,
+        value: state.value,
+        enqueue: (key) => {
+            if (key !== state.key) {
+                // console.log({ key, s: state.key })
+                console.log(key)
+                setState({
+                    ...state,
+                    key,
+                })
+            }
+            /* key !== state.key &&
+                setState({
+                    ...state,
+                    key,
+                    next,
+                }) */
+        },
+    }
+}
+
+const SelectSampleAdvert: FC<{ onChange: (advert: Advert | null) => void }> = ({
+    onChange,
+}) => {
+    const { listAdverts } = useContext(AdvertsContext)
+    const { value: adverts, enqueue } = useDebounce<AdvertList | null>(
+        null,
+        null,
+        () => listAdverts({ search: '' })
+    )
+
+    return (
+        <Autocomplete
+            options={adverts?.adverts || []}
+            renderOption={(props, advert) => (
+                <Typography key={advert.id} {...props}>
+                    {advert.title}
+                </Typography>
+            )}
+            onInputChange={(_, search) =>
+                enqueue(search, () => listAdverts({ search }))
+            }
+            onChange={(_, value) => onChange(value)}
+            getOptionLabel={(advert) => advert.title}
+            isOptionEqualToValue={(a, b) => a.id === b.id}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Sök annons"
+                    InputProps={{
+                        ...params.InputProps,
+                        type: 'search',
+                    }}
+                />
+            )}
+        />
+    )
+}
 
 export const EditSmsTemplatesForm: FC<{
     templates: SmsTemplate[]
@@ -30,17 +135,48 @@ export const EditSmsTemplatesForm: FC<{
             enabled,
         }))
     )
+
+    const [previewAdvert, setPreviewAdvert] = useState<Advert | null>(null)
+
+    const updatePreviewAdvert = useCallback(
+        (advert: Advert | null) => {
+            setPreviewAdvert(advert)
+        },
+        [setPreviewAdvert]
+    )
     const mutateTemplate = useCallback(
-        (t: SmsTemplate, patch: Partial<SmsTemplate>) =>
+        (t: SmsTemplate, patch: Partial<SmsTemplate>) => {
             setMemo(
                 memo.map((existing) =>
                     existing === t ? { ...existing, ...patch } : existing
                 )
-            ),
+            )
+        },
         [memo, setMemo]
     )
-    const saveTemplates = useCallback(() => onUpdate(memo), [onUpdate, memo])
 
+    const saveTemplates = useCallback(() => onUpdate(memo), [onUpdate, memo])
+    /*
+    useEffect(() => {
+        if (previewAdvert) {
+            preview.enqueue(
+                [
+                    previewAdvert.id,
+                    ...memo.map(({ templateId }) => templateId),
+                    ...memo.map(({ template }) => template),
+                ].join('@'),
+                () =>
+                    previewSmsTemplates(memo, previewAdvert).then((p) =>
+                        toMap(
+                            p,
+                            ({ templateId }) => templateId,
+                            (t) => t
+                        )
+                    )
+            )
+        }
+    })
+*/
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -53,6 +189,12 @@ export const EditSmsTemplatesForm: FC<{
                         <TableCell>
                             {phrase('SMS_TEMPLATE_FIELD_TEMPLATE', 'Mall')}
                         </TableCell>
+                        <TableCell>
+                            <SelectSampleAdvert
+                                onChange={updatePreviewAdvert}
+                            />
+                            [{previewAdvert?.id}]
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableFooter>
@@ -64,7 +206,7 @@ export const EditSmsTemplatesForm: FC<{
                 </TableFooter>
                 <TableBody>
                     {memo.map((t) => (
-                        <TableRow>
+                        <TableRow key={t.templateId}>
                             <TableCell>
                                 <Typography style={{ whiteSpace: 'nowrap' }}>
                                     {t.templateId}
@@ -111,6 +253,7 @@ export const EditSmsTemplatesForm: FC<{
                                     }
                                 />
                             </TableCell>
+                            <TableCell />
                         </TableRow>
                     ))}
                 </TableBody>
