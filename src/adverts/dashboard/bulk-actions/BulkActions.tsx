1736fd49ca3468272378daf1f86e07d2d7420edd
Joakim Larsson <mimmertim@gmail.com>
2024-02-28 10:59:29 +0100

    responsive bulk action buttons

--- src/adverts/dashboard/bulk-actions/BulkActions.tsx
+++ src/adverts/dashboard/bulk-actions/BulkActions.tsx
@@ -1,16 +1,30 @@
 import { FC } from 'react'
+import { Box, Stack, useMediaQuery, useTheme } from '@mui/material'
 import { BulkAction } from './types'
 import { BulkActionsAsDropdownButton } from './BulkActionsAsDropdownButton'
 import { BulkActionsAsButtonGroup } from './BulkActionsAsButtonGroup'
 
 export const BulkActions: FC<{ bulkActions: BulkAction[] }> = ({
     bulkActions,
-}) =>
-    bulkActions.length > 6 ? (
-        <BulkActionsAsDropdownButton
-            key="bulk-actions-as-dropdown"
-            bulkActions={bulkActions}
-        />
-    ) : (
-        <BulkActionsAsButtonGroup bulkActions={bulkActions} />
+}) => {
+    const theme = useTheme()
+    const largeScreen = useMediaQuery(theme.breakpoints.up('md'))
+    const manyButtons = bulkActions.length >= 6
+    const showCompact = manyButtons || !largeScreen
+    return (
+        <Stack direction="row">
+            <Box sx={{ flex: 1 }} />
+            {showCompact ? (
+                <BulkActionsAsDropdownButton
+                    key="bulk-actions-as-dropdown"
+                    bulkActions={bulkActions}
+                />
+            ) : (
+                <BulkActionsAsButtonGroup
+                    key="bulk-actions-as-button-group"
+                    bulkActions={bulkActions}
+                />
+            )}
+        </Stack>
     )
+}
