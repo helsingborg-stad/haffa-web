2b4b2414df6fbf0822a5947ab9e3a64df798fed5
Petter Andersson <petter.andersson@helsingborg.se>
2024-02-29 15:20:27 +0100

    fix loading of html branding

--- src/admin/html-branding/mappers.ts
+++ src/admin/html-branding/mappers.ts
@@ -4,7 +4,7 @@ export const getDefaultHtmlOptions = (): HtmlOptions => ({
     title: 'Haffa',
     description: 'Haffa - appen för återbruk',
     url: 'https://haffa.helsingborg.se',
-    imageFavicon: '/image-favicon.png',
-    imageLogo192: '/image-logo192.png',
-    imageLogo512: '/image-logo512.png',
+    imageFavicon: '',
+    imageLogo192: '',
+    imageLogo512: '',
 })
