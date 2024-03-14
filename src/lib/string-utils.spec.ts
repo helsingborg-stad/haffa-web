829937470d2980990532c261424ad343a647ef23
Petter Andersson <petter.andersson@helsingborg.se>
2024-03-14 10:05:47 +0100

    add test case

---
+++ src/lib/string-utils.spec.ts
@@ -0,0 +1,27 @@
+import { isValidUrl } from './string-utils'
+
+describe('isValidUrl', () => {
+    it('should accept common url-patterns', () => {
+        const list = [
+            'https://www.google.se',
+            'http://www.google.se',
+            'https://www.google.se/path',
+            'https://www.google.se/?path=path',
+        ]
+        list.forEach((u) => expect(isValidUrl(u)).toBe(true))
+    })
+    it('should deny invalid url-patterns', () => {
+        const list = [
+            'https://',
+            'http://',
+            'file://google.se',
+            'mailto:',
+            'tel:',
+            'www.google.se',
+            'https://a b',
+            '/google.se',
+            '',
+        ]
+        list.forEach((u) => expect(isValidUrl(u)).toBe(false))
+    })
+})
