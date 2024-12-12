50fdcd9fec8b0973425f652ca2f75343b4e9d45f
Petter Andersson <petter.andersson@helsingborg.se>
2024-12-12 12:52:17 +0100

    make tags on productcard clickable

--- src/adverts/components/details/advert-card/TagCard.tsx
+++ src/adverts/components/details/advert-card/TagCard.tsx
@@ -1,14 +1,25 @@
 import { Chip, PaperProps } from '@mui/material'
 import { Advert } from 'adverts'
+import { useNavigate } from 'react-router-dom'
 
 export const TagCard = (props: PaperProps & { advert: Advert }) => {
     const { tags } = props.advert
 
+    const nav = useNavigate()
+
+    const clicked = (tag: string) => {
+        nav(`/browse/#tags=${tag}`)
+    }
     return (
         tags.length > 0 && (
             <>
                 {tags.map((v) => (
-                    <Chip label={v} sx={{ mr: 1 }} />
+                    <Chip
+                        clickable
+                        onClick={() => clicked(v)}
+                        label={v}
+                        sx={{ mr: 1 }}
+                    />
                 ))}
             </>
         )
