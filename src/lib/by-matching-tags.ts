b107b63c5723a0210ddb514d0d220ec241bf7f50
Petter Andersson <petter.andersson@helsingborg.se>
2025-02-18 10:57:26 +0100

    fix bug where tags where visible on advert not having any

--- src/lib/by-matching-tags.ts
+++ src/lib/by-matching-tags.ts
@@ -4,5 +4,5 @@ export const byMatchingTags = <T extends { tag: string }>(
     tags: string[]
 ): Func1<T, boolean> => {
     const s = new Set(tags.map((t) => t?.trim() || '').filter((v) => v))
-    return s.size > 0 ? (elem) => s.has(elem.tag) : () => true
+    return s.size > 0 ? (elem) => s.has(elem.tag) : () => false
 }
