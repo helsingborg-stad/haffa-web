95d4c71a6c4fce218ea2a1586514e722c0b0cf3c
Petter Andersson <petter.andersson@helsingborg.se>
2025-04-02 12:00:16 +0200

    fix markdown h-sizes

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -3,11 +3,11 @@ import { FC } from 'react'
 import ReactMarkdown, { Components } from 'react-markdown'
 
 const components: Components = {
-    h1: ({ children }) => <Typography variant="h6">{children}</Typography>,
-    h2: ({ children }) => <Typography variant="h6">{children}</Typography>,
-    h3: ({ children }) => <Typography variant="h6">{children}</Typography>,
-    h4: ({ children }) => <Typography variant="h6">{children}</Typography>,
-    h5: ({ children }) => <Typography variant="h6">{children}</Typography>,
+    h1: ({ children }) => <Typography variant="h1">{children}</Typography>,
+    h2: ({ children }) => <Typography variant="h2">{children}</Typography>,
+    h3: ({ children }) => <Typography variant="h3">{children}</Typography>,
+    h4: ({ children }) => <Typography variant="h4">{children}</Typography>,
+    h5: ({ children }) => <Typography variant="h5">{children}</Typography>,
     h6: ({ children }) => <Typography variant="h6">{children}</Typography>,
     a: ({ children, href }) => <Link href={href}>{children}</Link>,
 }
