e880c1b3311060876e33056322dbca074e56709f
Petter Andersson <petter.andersson@helsingborg.se>
2026-08-25 10:45:24 +0200

    replace category tree browser

--- src/admin/categories/components/use-tree.tsx
+++ src/admin/categories/components/use-tree.tsx
@@ -1,16 +1,16 @@
-import type { TreeProps } from 'antd'
-import type { DataNode } from 'antd/es/tree'
+import { TreeItem } from '@mui/x-tree-view/TreeItem'
 import { createTreeAdapter } from 'lib/tree-adapter'
 import { treeDetach, treeFind, treeFindReplace } from 'lib/tree-lookup'
 import { getTreeNodeActions } from 'lib/tree-node-actions'
-import type { Action1, Action2, Action3, Func1 } from 'lib/types'
+import type { Action1, Action2, Func1 } from 'lib/types'
 import { uniqueBy } from 'lib/unique-by'
-import { useCallback, useEffect, useReducer } from 'react'
+import { type ReactNode, useCallback, useEffect, useReducer } from 'react'
 
 export interface TreeHookData<T> {
     nodes: T[]
     selectedNode: T | null
-    treeProps: TreeProps
+    treeViewProps: TreeViewProps
+    renderTreeItems: Func1<T[], ReactNode>
     addNode: Action1<T>
     updateNode: Action2<T, Func1<T, Partial<T>>>
     removeNode: Action1<T>
@@ -30,97 +30,38 @@ export interface TreeHookViewState {
     expandedKeys: Key[]
 }
 
+export interface TreeViewProps {
+    expandedItems: string[]
+    selectedItems: string | null
+    onExpandedItemsChange: (
+        event: React.SyntheticEvent,
+        itemIds: string[]
+    ) => void
+    onSelectedItemsChange: (
+        event: React.SyntheticEvent,
+        itemId: string | null
+    ) => void
+}
+
+type Key = string
+
 interface Model<T> {
     initialized: boolean
     nodes: T[]
     selectedNode: T | null
-    treeProps: TreeProps
+    treeViewProps: TreeViewProps
     expandedKeys: Key[]
     dispatch: React.Dispatch<Func1<Model<T>, Partial<Model<T>>>>
 }
 
-type Key = string | number | bigint
-
-const createTreeHandlerProps = <T>({
-    tree,
-    expand,
-    select,
-    move,
-    find,
-}: {
-    tree: DataNode[]
-    select: Action2<Key, boolean>
-    expand: Action2<Key, boolean>
-    move: Action3<Key, T, number>
-    find: Func1<Key, T | null>
-}): TreeProps => {
-    const onSelect: TreeProps['onSelect'] = (_, { selected, node: { key } }) =>
-        select(key, selected)
-
-    const onExpand: TreeProps['onExpand'] = (_, { expanded, node: { key } }) =>
-        expand(key, expanded)
-
-    const onDragEnter: TreeProps['onDragEnter'] = () => {}
-
-    const onDrop: TreeProps['onDrop'] = (info) => {
-        const dropKey = info.node.key
-        const dragKey = info.dragNode.key
-        const dropPos = info.node.pos.split('-')
-        const dropPosition =
-            info.dropPosition - Number(dropPos[dropPos.length - 1])
-
-        // Find dragobject and detach from tree
-        const dragObj = find(dragKey)
-        if (!dragObj) {
-            return {}
-        }
-
-        if (!info.dropToGap) {
-            // Drop on the content
-            move(dropKey, dragObj, 0)
-        } else if (
-            ((info.node as any).props.children || []).length > 0 && // Has children
-            (info.node as any).props.expanded && // Is expanded
-            dropPosition === 1 // On the bottom gap
-        ) {
-            move(dropKey, dragObj, 0)
-        } else {
-            move(dropKey, dragObj, dropPosition)
-        }
-    }
-    return {
-        treeData: tree,
-        selectable: true,
-        draggable: true,
-        onSelect,
-        onExpand,
-        onDragEnter,
-        onDrop,
-    }
-}
-
-const makeTree = <T>(
-    nodes: T[],
-    key: Func1<T, string>,
-    title: Func1<T, DataNode['title']>,
-    children: Func1<T, T[]>
-): DataNode[] =>
-    nodes.map((n) => ({
-        key: key(n),
-        title: title(n),
-        children: makeTree(children(n), key, title, children),
-    }))
-
-const buildModel = <T>(
+const buildModel = <T,>(
     nodes: T[],
     keyFn: Func1<T, string>,
-    titleFn: Func1<T, DataNode['title']>,
     childrenFn: Func1<T, T[]>,
     selectedKey: Key | null,
     expandedKeys: Key[],
     dispatch: Action1<Func1<Model<T>, Partial<Model<T>>>>
 ): Model<T> => {
-    const tree = makeTree(nodes, keyFn, titleFn, childrenFn)
     const selected = selectedKey
         ? treeFind(nodes, childrenFn, (n) => keyFn(n) === selectedKey)
         : null
@@ -130,76 +71,41 @@ const buildModel = <T>(
         nodes,
         selectedNode: selected ? selected.node : null,
         expandedKeys,
-        treeProps: {
-            expandedKeys,
-            selectedKeys: selected ? [keyFn(selected.node)] : [],
-            ...createTreeHandlerProps({
-                tree,
-                select: (key, selected) => {
-                    dispatch(({ expandedKeys }) => {
-                        const n = selected
-                            ? treeFind(
-                                  nodes,
-                                  childrenFn,
-                                  (n) => keyFn(n) === key
-                              )?.node || null
-                            : null
-                        const expand: Partial<Model<T>> =
-                            n && childrenFn(n).length > 0
-                                ? {
-                                      expandedKeys: [
-                                          ...new Set([
-                                              ...expandedKeys,
-                                              keyFn(n),
-                                          ]),
-                                      ],
-                                  }
-                                : {}
-                        return {
-                            selectedNode: n,
-                            ...expand,
-                        }
-                    })
-                },
-                expand: (key, expanded) =>
-                    dispatch(({ expandedKeys }) => ({
-                        expandedKeys: expanded
-                            ? [...expandedKeys, key]
-                            : expandedKeys.filter((k) => k !== key),
-                    })),
-                move: (key, node: T, index: number) =>
-                    dispatch(({ nodes }) => {
-                        // detach node before re-attaching
-                        if (treeDetach(nodes, childrenFn, (n) => n === node)) {
-                            const parent = treeFind(
-                                nodes,
-                                childrenFn,
-                                (n) => keyFn(n) === key
-                            )
-                            const pc = parent ? childrenFn(parent.node) : nodes
-                            if (index < 1) {
-                                pc?.splice(index, 0, node)
-                            } else {
-                                pc?.splice(index + 1, 0, node)
-                            }
-                            return {
-                                nodes: [...nodes],
-                            }
-                        }
-                        return {}
-                    }),
-                find: (key) =>
-                    treeFind(nodes, childrenFn, (n) => keyFn(n) === key)
-                        ?.node || null,
-            }),
+        treeViewProps: {
+            expandedItems: expandedKeys,
+            selectedItems: selected ? keyFn(selected.node) : null,
+            onExpandedItemsChange: (_event, itemIds) =>
+                dispatch(() => ({ expandedKeys: itemIds })),
+            onSelectedItemsChange: (_event, itemId) =>
+                dispatch(({ expandedKeys }) => {
+                    const n = itemId
+                        ? treeFind(
+                              nodes,
+                              childrenFn,
+                              (n) => keyFn(n) === itemId
+                          )?.node || null
+                        : null
+                    const expand: Partial<Model<T>> =
+                        n && childrenFn(n).length > 0
+                            ? {
+                                  expandedKeys: [
+                                      ...new Set([...expandedKeys, keyFn(n)]),
+                                  ],
+                              }
+                            : {}
+                    return {
+                        selectedNode: n,
+                        ...expand,
+                    }
+                }),
         },
     }
 }
 
-export const useTree = <T>(
+export const useTree = <T,>(
     initialNodes: T[],
     keyFn: Func1<T, string>,
-    titleFn: Func1<T, DataNode['title']>,
+    titleFn: Func1<T, ReactNode>,
     childrenFn: Func1<T, T[]>,
     viewState?: TreeHookViewState
 ): TreeHookData<T> => {
@@ -212,7 +118,6 @@ export const useTree = <T>(
             const x: Model<T> = buildModel(
                 patch.nodes,
                 keyFn,
-                titleFn,
                 childrenFn,
                 patch.selectedNode ? keyFn(patch.selectedNode) : null,
                 patch.selectedNode
@@ -232,8 +137,11 @@ export const useTree = <T>(
             initialized: false,
             nodes: [],
             selectedNode: null,
-            treeProps: {
-                treeData: [],
+            treeViewProps: {
+                expandedItems: [],
+                selectedItems: null,
+                onExpandedItemsChange: () => {},
+                onSelectedItemsChange: () => {},
             },
             expandedKeys: [],
             dispatch: (root) => root,
@@ -285,10 +193,28 @@ export const useTree = <T>(
         [model, childrenFn]
     )
 
+    const renderTreeItems = useCallback(
+        (nodes: T[]): ReactNode =>
+            nodes.map((n) => {
+                const children = childrenFn(n)
+                return (
+                    <TreeItem
+                        key={keyFn(n)}
+                        itemId={keyFn(n)}
+                        label={titleFn(n)}
+                    >
+                        {children.length > 0 ? renderTreeItems(children) : null}
+                    </TreeItem>
+                )
+            }),
+        [keyFn, titleFn, childrenFn]
+    )
+
     return {
         nodes: model.nodes,
         selectedNode: model.selectedNode,
-        treeProps: model.treeProps,
+        treeViewProps: model.treeViewProps,
+        renderTreeItems,
         viewState: {
             selectedKey: model.selectedNode ? keyFn(model.selectedNode) : '',
             expandedKeys: model.expandedKeys,
